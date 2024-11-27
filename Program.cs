4964c9422fa93f3677f097d9811db1321985ac8c
trsaints <thiago.dev2k3@gmail.com>
2024-11-27 09:18:25 -0300

    feat: create an endpoint for obtaining game details

--- Program.cs
+++ Program.cs
@@ -40,13 +40,37 @@ app.MapGet("/games", async ([FromQuery] string? queryParams) =>
     .WithName("GetGames")
     .WithOpenApi();
 
+app.MapGet("/games/{id:int}", async (int id) =>
+    {
+        using HttpClient client = new();
+
+        try
+        {
+            var routeUrl = ApiUtils.GetApiUrl(builder.Configuration, $"games/{id}");
+            var response = await client.GetAsync(routeUrl);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<object>(content);
+
+            return Results.Ok(result);
+        }
+        catch (Exception exception)
+        {   
+            Console.WriteLine("Oops! Something went wrong.");
+
+            return Results.BadRequest(exception.Message);
+        }
+    })
+    .WithName("GetGameById")
+    .WithOpenApi();
+
 app.Run();
 
 internal static class ApiUtils
 {
     public static string GetApiUrl(IConfiguration configuration,
         string route,
-        string? queryParams)
+        string? queryParams = "")
     {
         var apiKey = configuration["CLIENT_SECRET"];
         return $"https://api.rawg.io/api/{route}?token&key={apiKey}{queryParams}";
